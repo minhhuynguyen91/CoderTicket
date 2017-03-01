e78cc9d2ddea150866f6b7f60d33379fe550e69d
Minh Nguyen <ng.huyminh91@gmail.com>
2017-03-02 06:05:57 +0700

    Initial commit
    Signed-off-by: Minh Nguyen <ng.huyminh91@gmail.com>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,29 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all    
+    if params[:search].nil? || params[:search].blank?
+      @events = Event.all
+    else
+      @events = Event.search(params[:search])
+    end
   end
 
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @categories = Category.all
+    @event = Event.new
+    @event.build_venue
+  end
+
+  def create
+    
+  end
+
+  private
+    def event_params
+      params.require(:event).permit!
+    end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,19 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :extended_html_description, :venue, :category, :starts_at
   validates_uniqueness_of :name, uniqueness: {scope: [:venue, :starts_at]}
+
+  accepts_nested_attributes_for :venue
+
+  def self.search(search)
+    if search
+      where("name ILIKE ?", "%#{search}%") 
+    else
+      where
+    end
+  end
+
+  def self.upcoming
+    where("starts_at > ?",Time.now)
+  end
+
 end
