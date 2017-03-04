ef2e8bfe149cd09a4e967ab87c80c30d0ecb364b
Minh Nguyen <ng.huyminh91@gmail.com>
2017-03-04 12:23:02 +0700

    Fix the form for new ticket
    Signed-off-by: Minh Nguyen <ng.huyminh91@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,31 @@
 Rails.application.routes.draw do
 
+  get 'ownerships/create'
+
+  get 'ownerships/destroy'
+
   root 'events#index'
-  resources :users, :only => [:new, :create]
+  resources :users, :only => [:new, :create, :show]
   
+  #Sessions controller
   get  'signin' => 'sessions#new'
   post 'signin' => 'sessions#create'
-  get 'auth/:provider/callback' => 'sessions#callback'
-  
+  get 'auth/:provider/callback' => 'sessions#callback'  
   delete 'signout' => 'sessions#destroy'
 
-
-  resources :ticket_types, only: [:new, :create]
+  resources :ticket_types, :only => [:new, :create]
   
+  #resources :tickets, :only => [:new, :create]
+
   resources :events do
+    resources :tickets, :only => [:new, :create]
     collection do
       get 'mines' => 'events#event_list'
       post 'publish' => 'events#publish'
+
+      post 'add_owner' => 'ownerships#create'
+      delete 'remove_owner' => 'ownerships#destroy'
     end
-    resources :tickets
   end
 
 end
